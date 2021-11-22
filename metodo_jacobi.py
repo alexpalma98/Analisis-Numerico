82587e250f0153b9272575b75349efd1abf116f1
alexpalma98 <83353323+alexpalma98@users.noreply.github.com>
2021-11-22 10:05:46 -0500

    Update metodo_jacobi.py

--- metodo_jacobi.py
+++ metodo_jacobi.py
@@ -1,1 +1,6 @@
+#El método de Jacobi realiza operaciones semejantes al método de Gauss-Seidel.
+#El método de Jacobi también usa el vector inicial X0, la diferencia consiste en que la actualización del vector X
+#en cada iteración se realiza cuando se ha calculado el vector nuevo completo.
+
+#ESCRIBIR EJEMPLO
 
